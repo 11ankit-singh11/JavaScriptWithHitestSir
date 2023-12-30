997c42ebccbc48a0544907b326bb09bf862d34cc
Ankit Singh <124033231+11ankit-singh11@users.noreply.github.com>
2023-12-30 10:22:12 +0000

    learned datatyes along with variables

---
+++ basics01/variables.js
@@ -0,0 +1,24 @@
+let accemail = "as@gmail.com"
+const accid = "abcd"
+var accpass = "1849"
+let ankit 
+// assigning new value to a constant variable will throw error
+console.table([accemail,accid,accpass,ankit])
+// use of var is should be ignored because it create problem in scoping
+console.log(typeof ankit)
+// undefined is the type of undefined
+// object is the type of null it is used when we dont want to yet give the value 
+// we got many datatypes like numbers, string,null, undefined , boolean, bigint
+console.log(typeof "ankit")
+console.log(typeof null)
+let arr = null
+console.log(typeof arr)
+
+
+
+
+
+
+
+
+

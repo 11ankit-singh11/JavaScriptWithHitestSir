8fa44d0b1f5cf26a5e8177a5f6dca5ae8d0e6b02
Ankit Singh <124033231+11ankit-singh11@users.noreply.github.com>
2023-12-30 12:20:44 +0000

    learned conversion and operations in js

---
+++ basics01/conversionoperation.js
@@ -0,0 +1,24 @@
+// let a = "22abc"
+// console.log(typeof a)
+// console.log(a)
+// let b = Number(a)
+// console.log(b)
+// console.log(typeof b)
+// // NaN not a number but still it will give number
+// let c = 2
+// let d = Boolean(c)
+// console.log(d)
+// let p = Boolean("ankit")
+// console.log(p)
+// let q = Boolean("")
+// console.log(q)
+
+
+//****************************************OPERATIONS***************************************************
+console.log(1+2+"3")
+console.log("3"+4+4)
+console.log(+true)
+// true+ will give error 
+console.log(+"")
+console.log(4+true)
+console.log(4+false)

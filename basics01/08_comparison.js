6dca68b8b8babfde23531ad27dbd2e9d916ee8d3
Ankit Singh <124033231+11ankit-singh11@users.noreply.github.com>
2024-05-22 11:57:03 +0000

    hehe

---
+++ basics01/08_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+//  console.log(2<1);
+
+// comparing two different datatypes 
+
+//  equality operator like <= , >= or == change the null into 0 and then do the comarison
+
+// console.log(null==0);
+// console.log(null<0);
+// console.log(null>0);
+// console.log(null>=0);
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0); // all false related to undefined
+
+//  strict check 
+
+// console.log("2"===2) -> false as its checking datatype also 
+// console.log("2"==2)   -> true normal check 
+

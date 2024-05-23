63c34cbae5654c30d3f31e678b77c765b6350415
Ankit Singh <124033231+11ankit-singh11@users.noreply.github.com>
2024-05-23 17:18:28 +0000

    ne

---
+++ 10_memory.js
@@ -0,0 +1,8 @@
+// memory are of two types 
+// stack(primitive) , all primitive values go to stack and copy of these are generated not actual refrence is created .
+// for example
+a = "ankit"
+b = a 
+b = " SIngh"
+console.log(a)
+console.log(b)

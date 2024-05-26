22e911d1cb58788a390a160617b9ae5dca5df60d
Ankit Singh <124033231+11ankit-singh11@users.noreply.github.com>
2024-05-26 13:36:57 +0000

    completed

---
+++ basics01/013_datesinjs.js
@@ -0,0 +1,34 @@
+// let mydate = new Date();
+// console.log(mydate)
+// console.log(mydate.toString())
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleString())
+// console.log(mydate.toISOString())
+// console.log(typeof(mydate))
+
+// let anotherdate = new Date(2023,0,23);
+// let anotherdateq= new Date(2023,0,23,3,2,0);
+// console.log(anotherdate.toDateString())
+// console.log(anotherdate.toLocaleString())
+// let abc = new Date("01-23-2024")
+// console.log(abc)
+
+
+// time in millisecond from 1970
+
+// let time = Date.now()
+// console.log(time)
+// console.log(anotherdate.getTime())
+
+// some other functions 
+
+let ankit = new Date();
+console.log(ankit.getDay())
+console.log(ankit.getDate())
+console.log(ankit.getMonth())
+
+console.log(ankit.toLocaleString('default',{
+    weekday:"long"
+    
+}))
+

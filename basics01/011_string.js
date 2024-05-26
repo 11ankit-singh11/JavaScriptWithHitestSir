031594978afe328ff6473a8d4b2a0f10f63aef60
Ankit Singh <124033231+11ankit-singh11@users.noreply.github.com>
2024-05-26 11:14:13 +0000

    noi

---
+++ basics01/011_string.js
@@ -0,0 +1,15 @@
+console.log("ankit")
+a = "ankit"
+age = "22"
+console.log(`hello my name is ${a} and my age is ${age}`)
+// this is called string interpolation
+console.log(a[0])
+// character and index are like key value pair 
+const gamename = new String('hiteshchaudhary')
+console.log(gamename[3]);
+console.log(gamename.__proto__); // it will give {} which represents object
+console.log(gamename.charAt(4))
+console.log(gamename.indexOf('c'))
+console.log(gamename.toUpperCase())
+console.log(gamename.includes('chaud'))
+// other functions are also there like trim() , replace("to be searched ","to be replaced with"), split and u can take help from mdn

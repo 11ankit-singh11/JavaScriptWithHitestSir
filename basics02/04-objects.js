92fdd95e89204bd1ef6e38afbcaa659a54419090
Ankit Singh <124033231+11ankit-singh11@users.noreply.github.com>
2024-06-04 11:19:32 +0000

    fone

---
+++ basics02/04-objects.js
@@ -0,0 +1,21 @@
+// const tinder = new object();        it is singleton object 
+
+const tinder = {} ; //it is also object but it is not singleton just naming 
+
+tinder.id = "2343"
+tinder.name = "ankit"
+console.log(tinder);
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",5:"b"}
+const obj3 = {obj1,obj2} // this will add two objects in obj3 but actual adding of all elements in sequence will be missed 
+
+const obj4 = Object.assign({},obj2,obj1)
+console.log(obj4);
+
+// best method 
+const obj5 = {...obj1,...obj2}
+console.log(obj5);
+
+console.log(Object.keys(obj5));
+console.log(Object.entries(obj5))

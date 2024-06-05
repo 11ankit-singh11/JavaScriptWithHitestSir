a21a0228a5e3ce38f69ac1c21c87efe4c8167f75
Ankit Singh <124033231+11ankit-singh11@users.noreply.github.com>
2024-06-05 09:54:18 +0000

    dilli

---
+++ basics03/01functions.js
@@ -0,0 +1,61 @@
+// function saymyname() {
+//     console.log("a")
+//     console.log("N")
+//     console.log("k")
+//     console.log("i")
+//     console.log("t")
+// }
+// saymyname()
+
+// function add(number1,number2){ // parameters 
+//     console.log(number1+number2)
+// }
+// add(3,"a") // arguments 
+
+// function adding(n,a){
+//     return n+a
+// }
+// const aa = adding(3,5)
+// console.log("result:",aa)
+
+
+function username(usr){
+    if(!usr){
+        console.log("invalid name")
+        return 
+    }
+      console.log(`${usr} just logged in`)
+}
+username()
+
+
+// rest operator 
+
+// function num(...num1){
+//     console.log(num1)
+// }
+// num(3,3,5,2,2,25)
+
+
+
+// functions and objects 
+
+// function num(a, b ,...num1){
+//     console.log(num1)
+// }
+// num(3,3,5,2,2,25)
+
+// function anyobject(ankit){
+//     console.log(ankit.ek)
+// }
+// const obj = {
+//     ane : "ankit",
+//     ek :  33
+// }
+// anyobject(obj)
+
+//  Or we can directly pass object like anyobject({name:"ankit",ek:33})
+
+
+
+

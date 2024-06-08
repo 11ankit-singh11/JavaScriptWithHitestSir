75e31ee9ed3013b7cefd764aa46bfe75f38d2b63
Ankit Singh <124033231+11ankit-singh11@users.noreply.github.com>
2024-06-08 09:48:17 +0000

    control

---
+++ basics03/05controlflow.js
@@ -0,0 +1,24 @@
+// if else same as java and && || also same 
+
+// switch also same as java if break is not there all will run except default 
+const month = 3;
+switch (month) {
+    case 1: console.log("january")
+        
+        break;
+    case 2: console.log("january")
+        
+        break;
+    case 3: console.log("march")
+        
+        break;
+    case 4: console.log("january")
+        
+        break;
+    case 5: console.log("january")
+        
+        break;
+
+    default: console.log("none")
+        break;
+}

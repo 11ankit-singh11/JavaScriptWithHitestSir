cf34b441836feb12414bb7303fa8e189ccd8d680
Ankit Singh <124033231+11ankit-singh11@users.noreply.github.com>
2024-06-08 09:54:09 +0000

    nnn

---
+++ basics03/06truthy.js
@@ -0,0 +1,7 @@
+// falsy values -> values considered false in conditional and also in general 
+
+false,0,-0,BigInt On,"",null,undefined,NaN
+
+// Truthy values 
+
+"0",'false'," ",[]=empty Array,{}, function(){}

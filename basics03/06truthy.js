4a3cf874b4c8fb94cd49754a1ff4a42c1e474712
Ankit Singh <124033231+11ankit-singh11@users.noreply.github.com>
2024-06-08 10:13:09 +0000

    nid

--- basics03/06truthy.js
+++ basics03/06truthy.js
@@ -1,7 +1,20 @@
-// falsy values -> values considered false in conditional and also in general 
+// // falsy values -> values considered false in conditional and also in general 
 
-false,0,-0,BigInt On,"",null,undefined,NaN
+// false,0,-0,BigInt On,"",null,undefined,NaN
 
-// Truthy values 
+// // Truthy values 
 
-"0",'false'," ",[]=empty Array,{}, function(){}
+// "0",'false'," ",[]=empty Array,{}, function(){}
+
+// Nullish Coalescing  operator ?? used for two possibility sceneario means which value to be accepted 
+
+let val1 
+val1 = 5 ?? 10
+val1 = null ?? 20
+val1 = undefined ?? 15 
+console.log(val1)
+
+//  ternary operator   condition ? true : false 
+
+let name = "ankit"
+name=="ankit" ? console.log(true) : console.log(false);
